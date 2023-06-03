3fdbdbd527850ccd5165d610b0af82d97c134a9d
LUAN BRENDON <luan_brendonmacedo@hotmail.com>
2023-06-03 17:24:39 -0300

    Mudancas foram feitas e erros aconteceram (tem que arrumar o erro do troco)

--- src/FastFoodService.java
+++ src/FastFoodService.java
@@ -3,14 +3,9 @@ import java.rmi.RemoteException;
 import java.util.List;
 
 public interface FastFoodService extends Remote {
-    boolean login(String username, String password) throws RemoteException;
-    List<String> getAvailableItems() throws RemoteException;
-    void selectDeliveryAddress(String address) throws RemoteException;
-    void addItem(String item) throws RemoteException;
-    double checkout() throws RemoteException;
-    void logout() throws RemoteException;
-    String getDeliveryAddress() throws RemoteException;
-    List<String> getSelectedItems() throws RemoteException;
-    double getPrice(String item) throws RemoteException;
-    double processPayment(String clientName, double amountPaid) throws RemoteException;
+    List<Produto> getItensDisponiveis() throws RemoteException;
+    void selectItem(Produto produto) throws RemoteException;
+    List<Produto> getSelectedItems() throws RemoteException;
+    double getTotalAmount() throws RemoteException;
+    void pay(double amountPaid) throws RemoteException;
 }
--- src/ServerMain.java
+++ src/ServerMain.java
@@ -1,12 +1,21 @@
 import java.rmi.registry.LocateRegistry;
 import java.rmi.registry.Registry;
+import java.util.ArrayList;
+import java.util.List;
 
 public class ServerMain {
     public static void main(String[] args) {
         try {
-            FastFoodService fastFoodService = new FastFoodServer();
+            // Cria uma lista de objetos Produto para representar os itens disponíveis na lanchonete
+            List<Produto> itensDisponiveis = new ArrayList<>();
+            itensDisponiveis.add(new Produto("Hambúrguer", 10.0));
+            itensDisponiveis.add(new Produto("Batata Frita", 5.0));
+            itensDisponiveis.add(new Produto("Refrigerante", 3.0));
+
+            FastFoodService fastFoodService = new FastFoodServiceImpl(itensDisponiveis);
             Registry registry = LocateRegistry.createRegistry(4444);
             registry.rebind("FastFoodService", fastFoodService);
+
             System.out.println("Servidor pronto para receber solicitações.");
         } catch (Exception e) {
             System.err.println("Erro no servidor: " + e.toString());
